44c7fdf58991d64fc6ab185e307430375a2ee01a
Priyanshu T <priyanshu9943@gmail.com>
2024-02-06 23:55:11 +0530

    Day 05 of 30 node js challenge

---
+++ day-05.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+function checkFileExtension(filePath, expectedExtension) {
+  const extension = path.extname(filePath);
+
+  if (extension === expectedExtension) {
+    console.log(`File has the expected extension: ${expectedExtension}`);
+  }
+  
+  else {
+    console.log(
+      `File does not have the expected extension. Expected: ${expectedExtension}, Actual: ${extension}`
+    );
+  }
+}
+
+checkFileExtension("test-files/file1.txt", ".txt");
+// Expected Output: File has the expected extension: .txt
+
+checkFileExtension("test-files/image.png", ".jpg");
+// Expected Output: File does not have the expected extension. Expected: .jpg, Actual: .png
